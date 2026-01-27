bfd5f2d6603561e8400834e3eb69810dc2b4fe84
notSmart-Fr <yasirsamin4@gmail.com>
2026-01-27 16:48:46 +0600

    Added docker

--- app/core/config.py
+++ app/core/config.py
@@ -1,9 +1,10 @@
 from pydantic_settings import BaseSettings
 
-
 class Settings(BaseSettings):
     app_name: str = "RentWise API"
     database_url: str = "sqlite:///./rentwise.db"
 
+    class Config:
+        env_file = ".env"
 
 settings = Settings()
